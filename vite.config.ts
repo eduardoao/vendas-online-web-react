855fd1994f861890dd8277a7c49edc521962789e
Eduardo <eoalcantara@gmail.com>
2023-06-21 15:42:00 -0300

    Upload project with Dockefile

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+//https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+export default defineConfig({
+  plugins: [react()],
+  server : { 
+        watch : { 
+          usePolling : true , 
+        },
+        host : true , // necessário para o mapeamento de portas do Docker Container funcionar 
+        strictPort : true , 
+        port : 5173 , // você pode substituir esta porta por qualquer porta
+      }
+})
