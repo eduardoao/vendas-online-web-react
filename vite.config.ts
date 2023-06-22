a419d0fcc6f06f99bf39344bdddbace38d03248c
Eduardo <eoalcantara@gmail.com>
2023-06-22 10:29:45 -0300

    added esint and prettier

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 //https://vitejs.dev/config/
 // export default defineConfig({
@@ -8,12 +8,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server : { 
-        watch : { 
-          usePolling : true , 
-        },
-        host : true , // necessário para o mapeamento de portas do Docker Container funcionar 
-        strictPort : true , 
-        port : 5173 , // você pode substituir esta porta por qualquer porta
-      }
-})
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    host: true, // necessário para o mapeamento de portas do Docker Container funcionar
+    strictPort: true,
+    port: 5173, // você pode substituir esta porta por qualquer porta
+  },
+});
